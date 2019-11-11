de7c600cb38340ac981e68b77e3cf60e80d73ddf
Thomas Bye <thomas@rare.com.au>
2019-11-12 09:40:24 +1100

    webhook stuff

--- index.php
+++ index.php
@@ -27,6 +27,51 @@ require(ROOT . 'secure/config.php');
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// WEBHOOK ///////////////////////////////
+
+/* validate verify token needed for setting up web hook */
+if (isset($_GET['hub_verify_token'])) {
+    if ($_GET['hub_verify_token'] === 'my_stupid_verify_token') {
+        echo $_GET['hub_challenge'];
+        return;
+    } else {
+        echo 'Invalid Verify Token';
+        return;
+    }
+}
+
+/* receive and send messages */
+$input = json_decode(file_get_contents('php://input'), true);
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
+    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
+
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHQIruxo84BAEFv4dNPCybOTUHz7sD6illKMcoW1xghbuZCdYNPV5qLqhKt7zafINZBlrcameSq9fJYJ2YZB9ewt27s6BrJFn46nPLZAKwD1IhagzK9UEzOeTE60tMGiYkvzxHAFCVRmPs2Lu7Egcg6razk5M79DiTLZCvWHEQZDZD';
+
+    /*initialize curl*/
+    $ch = curl_init($url);
+    /*prepare response*/
+    $jsonData = '{
+    "recipient":{
+        "id":"' . $sender . '"
+        },
+        "message":{
+            "text":"You said, ' . $message . '"
+        }
+    }';
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    if (!empty($message)) {
+        $result = curl_exec($ch); // user will get the message
+    }
+    
+}
+
+
+
 // LOGIC ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //set default logic response
@@ -99,10 +144,11 @@ if (!empty($_POST['action'])) {
             'Odds' => $_POST['odds'],
             'Description' => $desc,
             'Result' => 'Pending',
-            'BonusBet' => 'test',
+            'Club' => 1,
+            'BonusBet' => $bb,
             'Date' => $date->format('Y-m-d H:i:s')
         );
-        print_r($insert_data);
+        //print_r($insert_data);
         $insert_result = $mysqli_db->insert('bets', $insert_data);
     }
 }
@@ -440,7 +486,7 @@ define('FOOT_JS', '
     '.ROOT.'web/script/index.page.js,
     '.ROOT.'web/script/bootstrap.min.js,
     '.ROOT.'web/script/datatables.js,
-    '.ROOT.'web/script/dataTables.responsive.min.js
+    '.ROOT.'web/script/dataTables.responsive.min.js,
 ');
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
